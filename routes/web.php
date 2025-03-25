96f017c65150d1d2aa93670df49b599192269ecf
Anaprincs <hipolitocarolinaana@gmail.com>
2025-03-25 10:19:43 -0300

    Eu criei o login, o livewire do cliente, e o blade do login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Livewire\Auth\Login;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', Login::class);
+
+Route::get('/clientes', function () {
+    return Auth::user();
+})->middleware('auth', 'role:cliente')->name('cliente.dashboard');
